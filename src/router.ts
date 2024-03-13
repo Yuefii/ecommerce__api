4bd4a1a3570cfd7baf3dd53a03edcda065a5eedd
yuefii <muffidahmadiawan31@gmail.com>
2024-03-13 16:55:57 +0700

    starter project

---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {
+    createUserController,
+    deleteUserController,
+    getUserByIdController,
+    getUsersController,
+    updateUserController
+} from './controllers/users.controller';
+
+export const router = express.Router()
+
+router.post('/v1/users/register', createUserController);
+router.get('/v1/users', getUsersController);
+router.get('/v1/users/:id', getUserByIdController);
+router.patch('/v1/users/:id/update', updateUserController);
+router.delete('/v1/users/:id/delete', deleteUserController);
+
---
+++ src/services/users.services.ts
@@ -0,0 +1,95 @@
+import prisma from "../lib/prisma";
+
+export const createUserService = async (userData: { 
+    nama: string, 
+    email: string, 
+    password: string, 
+    alamat: string, 
+    no_telp: string 
+}) => {
+    try {
+        const { 
+            nama, 
+            email, 
+            password, 
+            alamat, 
+            no_telp 
+        } = userData;
+        const result = await prisma.users.create({
+            data: {
+                email,
+                nama,
+                password,
+                alamat,
+                no_telp
+            },
+        });
+        return result;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getUsersService = async () => {
+    try {
+        const users = await prisma.users.findMany();
+        return users;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getUserByIdService = async (userId: string) => {
+    try {
+        const user = await prisma.users.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateUserService = async (userId: string, userData: { 
+    nama: string, 
+    email: string,  
+    alamat: string, 
+    no_telp: string 
+}) => {
+    try {
+        const { 
+            nama, 
+            email,  
+            alamat, 
+            no_telp 
+        } = userData;
+        const user = await prisma.users.update({
+            where: {
+                userId: userId,
+            },
+            data: {
+                nama,
+                email,
+                alamat,
+                no_telp
+            },
+        });
+        return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deleteUserService = async (userId: string) => {
+    try {
+        await prisma.users.delete({
+            where: {
+                userId: userId,
+            },
+        });
+    } catch (error) {
+        throw error;
+    }
+};
