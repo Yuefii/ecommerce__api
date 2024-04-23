9edcbfd73e1019f9bdf4b07c6b193f06e8a6b1ba
yuefii <muffidahmadiawan31@gmail.com>
2024-04-23 11:23:22 +0700

    bug: response error on productbyid

--- src/features/products/controllers/getProductByProductIdController.ts
+++ src/features/products/controllers/getProductByProductIdController.ts
@@ -6,13 +6,13 @@ export const getProductByProductIdController = async (req: Request, res: Respons
     try {
         const productId = req.params.productId;
         const product = await getProductByIdService(productId);
-        if (!product) {
-            return res.status(404).json({ error: 'product not found' });
-        }
         const response = ProductDTO(product)
         res.status(200).json({ data: response });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(404).json({
+            statusCode: 404,
+            error: "product not found"
+        });
     }
 };
