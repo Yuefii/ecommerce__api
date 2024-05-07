c87427d6b2cec2e50264e5eb7a9db2152d140c49
yuefii <muffidahmadiawan31@gmail.com>
2024-05-07 07:05:58 +0700

    refactor: add interface type data on discus DTO

--- src/dto/DiscusDto.ts
+++ src/dto/DiscusDto.ts
@@ -1,21 +1,63 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 
+interface discusTypeInterface {
+  name: string
+}
+
+interface replyInterface {
+  replyId?: string
+  discusId?: string | null
+  userId?: string | null
+  Users: any
+  name?: string
+  message?: string
+  createdAt?: Date
+}
+
+interface FromGetType {
+  discusId: string
+  userId?: string | null
+  message: string
+  Users: any
+  discusType: discusTypeInterface[]
+  reply?: replyInterface[]
+  createdAt: Date
+}
+
+interface FromCreateType {
+  discusId: string
+  userId?: string | null
+  message: string
+  Users: any
+  discusType: discusTypeInterface[]
+  createdAt: Date
+}
+
+interface FromReplyType {
+  replyId: string
+  discusId?: string | null
+  userId?: string | null
+  Users: any
+  message: string
+  createdAt: Date
+}
+
 export class DiscusDTO {
   constructor() {}
 
-  public fromGet(result: any) {
+  public fromGet(result: FromGetType) {
     return {
       discus_id: result.discusId,
       user_id: result.userId,
       discus_message: result.message,
       name: result.Users.nama,
       discus_type: Array.isArray(result.discusType)
-        ? result.discusType.map((item: any) => ({
+        ? result.discusType.map((item) => ({
             name: item.name
           }))
         : [],
       discus_reply: Array.isArray(result.reply)
-        ? result.reply.map((item: any) => ({
+        ? result.reply.map((item) => ({
             discus_id: item.discusId,
             reply_id: item.replyId,
             user_id: item.userId,
@@ -28,14 +70,14 @@ export class DiscusDTO {
     }
   }
 
-  public fromCreate(result: any) {
+  public fromCreate(result: FromCreateType) {
     return {
       discus_id: result.discusId,
       user_id: result.userId,
       name: result.Users.nama,
       discus_message: result.message,
       discus_type: Array.isArray(result.discusType)
-        ? result.discusType.map((item: any) => ({
+        ? result.discusType.map((item) => ({
             name: item.name
           }))
         : [],
@@ -43,7 +85,7 @@ export class DiscusDTO {
     }
   }
 
-  public fromReply(result: any) {
+  public fromReply(result: FromReplyType) {
     return {
       reply_id: result.replyId,
       discus_id: result.discusId,
