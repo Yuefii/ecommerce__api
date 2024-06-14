65060271f1cbe113fe932fc7c584e39d3edd2bec
yuefii <muffidahmadiawan31@gmail.com>
2024-06-14 19:05:04 +0700

    bug on types products -> discus -> users

--- src/dto/products/product-dto.ts
+++ src/dto/products/product-dto.ts
@@ -27,27 +27,39 @@ export type ProductResponse = {
     name: string | null
     username: string | null
     avatar: string | null
-  },
+  }
   review: Array<{
     review_id: string
-    comment: string,
-    rating: number,
+    comment: string
+    rating: number
     users: {
       user_id: string
       name: string | null
       username: string | null
       avatar: string | null
-    },
+    }
   }>
   discus: Array<{
     discus_id: string
     discus_message: string
+    // users: {
+    //   user_id: string
+    //   name: string | null
+    //   username: string | null
+    //   avatar: string | null
+    // }
     discus_type: Array<{
       name: string
     }>
     discus_reply: Array<{
       reply_id: string
       reply_message: string
+      // users: {
+      //   user_id: string
+      //   name: string | null
+      //   username: string | null
+      //   avatar: string | null
+      // }
       created_at: Date
     }>
     created_at: Date
@@ -96,7 +108,13 @@ export function toProductResponse(
     images: Images[]
     owner: Users
     review: (Reviews & { users: Users })[]
-    discus: (Discus & { discusType: DiscusType[]; reply: Reply[] })[]
+    discus: (Discus & {
+      // users: Users
+      discusType: DiscusType[]
+      reply: Reply[]
+      /*  types yang error */
+      /* reply: (Reply & { users: Users })[] */
+    })[]
   }
 ): ProductResponse {
   return {
@@ -118,30 +136,46 @@ export function toProductResponse(
       username: product.owner.username,
       avatar: product.owner.imageUrl
     },
-    review: product.review ? product.review.map((item) => ({
-      review_id: item.reviewId,
-      comment: item.comment,
-      rating: item.rating,
-      users: {
-        user_id: item.users.userId,
-        name: item.users.name,
-        username: item.users.username,
-        avatar: item.users.imageUrl
-      }
-    })) : [],
-    discus: product.discus ? product.discus.map((item) => ({
-      discus_id: item.discusId,
-      discus_message: item.message,
-      discus_type: item.discusType.map((item) => ({
-        name: item.name
-      })),
-      discus_reply: item.reply.map((item) => ({
-        reply_id: item.replyId,
-        reply_message: item.message,
+    review: product.review
+      ? product.review.map((item) => ({
+        review_id: item.reviewId,
+        comment: item.comment,
+        rating: item.rating,
+        users: {
+          user_id: item.users.userId,
+          name: item.users.name,
+          username: item.users.username,
+          avatar: item.users.imageUrl
+        }
+      }))
+      : [],
+    discus: product.discus
+      ? product.discus.map((item) => ({
+        discus_id: item.discusId,
+        discus_message: item.message,
+        // users: {
+        //   user_id: item.users.userId,
+        //   name: item.users.name,
+        //   username: item.users.username,
+        //   avatar: item.users.imageUrl
+        // },
+        discus_type: item.discusType.map((item) => ({
+          name: item.name
+        })),
+        discus_reply: item.reply.map((item) => ({
+          reply_id: item.replyId,
+          reply_message: item.message,
+          // users: {
+          //   user_id: item.users.userId,
+          //   name: item.users.name,
+          //   username: item.users.username,
+          //   avatar: item.users.imageUrl
+          // },
+          created_at: item.createdAt
+        })),
         created_at: item.createdAt
-      })),
-      created_at: item.createdAt
-    })) : [],
+      }))
+      : [],
     created_at: product.createdAt,
     updated_at: product.updateAt
   }
