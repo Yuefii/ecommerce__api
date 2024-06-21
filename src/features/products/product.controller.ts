b9651ffd9cf78f3d3c1c105429c50c8a2c03d7c7
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-06-21 09:42:34 +0700

    fix double imports

--- src/features/products/product.controller.ts
+++ src/features/products/product.controller.ts
@@ -3,7 +3,6 @@ import { ProductService } from './product.service'
 import { NextFunction, Request, Response } from 'express'
 
 import * as dto from '../../dto/products/product-dto'
-import { UploadedFile } from 'express-fileupload'
 
 export class ProductController {
   static async create(req: Request, res: Response, next: NextFunction) {
